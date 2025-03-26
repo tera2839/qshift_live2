e2089f548ec708cec6e496c0fa3c3e1deda457f7
tera2839 <ten283907@gmail.com>
2025-03-26 11:31:47 +0900

    初コミット

---
+++ src/main/java/com/quickshift/repository/MemberRequestRepository.java
@@ -0,0 +1,18 @@
+package com.quickshift.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.quickshift.entity.Calendar;
+import com.quickshift.entity.Member;
+import com.quickshift.entity.MemberRequest;
+import com.quickshift.entity.Store;
+import com.quickshift.entity.Timeplan;
+
+@Repository
+public interface MemberRequestRepository extends JpaRepository<MemberRequest, Long>{
+	List<MemberRequest> findByMember(Member member);
+	List<MemberRequest> findByCalendarAndStoreAndTimeplan(Calendar calendar, Store store, Timeplan timeplan);
+}
